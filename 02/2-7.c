28685dc9e12a031f14af2c89bd666ff3892782c6
isabellawuu <wuqianmei1997@gmail.com>
2018-04-16 16:39:28 +0900

    2-7

--- 02/2-7.c
+++ 02/2-7.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
 int main(void){
+  int t;
+  double x, r, k;
+  r=1; k=3;
+
   FILE *fp;
   fp=fopen("output.dat", "a");
+  fprintf(fp, "%d, %d", 0, x0);
 
-  fprintf(fp, "%d, %d", 0);
-
-  for(t=0; t<300; t++);
-  xx=x+r*(1-x/k)*x;
-  fprintf(fp, "%d, %d, t, xx");
+  for(t=0; t<300; t++){
+    x=xx
+    xx=x+r*(1-x/k)*x;
+    fprintf(fp, "%d, %d, t, xx");
   }
 
   fclose(fp);
