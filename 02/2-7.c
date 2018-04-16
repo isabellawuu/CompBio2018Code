4b0bf6167e2deccd7102d48cd35ad01862934956
isabellawuu <wuqianmei1997@gmail.com>
2018-04-16 16:17:31 +0900

    2-7

---
+++ 02/2-7.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+  FILE *fp;
+  fp=fopen("output.dat", "a");
+
+  fprintf(fp, "%d, %d", 0);
+
+  for(t=0; t<300; t++);
+  xx=x+r*(1-x/k)*x;
+  fprintf(fp, "%d, %d, t, xx");
+  }
+
+  fclose(fp);
+
+  return 0;
+}
